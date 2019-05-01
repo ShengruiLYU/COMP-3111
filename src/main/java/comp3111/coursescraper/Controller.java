2bc3ecf0eff897efa8a6d46befeaec704d0d4e25
ShengruiLYU <slvaa@connect.ust.hk>
2019-05-01 19:41:53 +0800

    improve description of controller

--- src/main/java/comp3111/coursescraper/Controller.java
+++ src/main/java/comp3111/coursescraper/Controller.java
@@ -53,7 +53,8 @@ import java.util.List;
  * All the following functions (exception initialization of the controller)are private, therefore they won't show in the javadoc. (Acoording to piazza Kevin's reply, we only show the public and protected functions)
  * search() is for the search tab, click search button will call this function and display the search result in the ui console.
  * allSubjectSearch() is for the all subject search tab, click all subject search buttion will display the all subject search result in the console and update progress bar.
- * printTimetable() is for displaying the courses passed in on the timetable.
+ * printTimetable2() is for displaying the timeslots passed in on the timetable.
+ * updateTimetable() will be called if timetable tab is clicked. It will fetech all the enrolled section list and display them.
  * checkBox() will update the filter selection and the console display whe the user change the filter options.
  * checkFlag() will check whether a course will be put in the updated course list according to the filter options.
  * findInstructorSfq() find the sfq for the instructor
